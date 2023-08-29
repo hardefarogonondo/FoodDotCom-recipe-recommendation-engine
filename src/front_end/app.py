0eb86682666a4e69da5efbf6837e03a2f33a04c3
hardefarogonondo <hardefarogonondo@gmail.com>
2023-08-30 03:42:02 +0700

    Frontend updated

--- src/front_end/app.py
+++ src/front_end/app.py
@@ -1,29 +1,47 @@
+# Import Libraries
+import requests
 import streamlit as st
 
 
-def get_user_rated_recipes(user_id):
-    return ["Recipe A", "Recipe B", "Recipe C", "Recipe D", "Recipe E", "Recipe F", "Recipe G", "Recipe H", "Recipe I", "Recipe J", "Recipe K", "Recipe L"]
+def get_available_users():
+    response = requests.get('http://127.0.0.1:8000/available_users')
+    if response.status_code == 200:
+        return response.json()["user_ids"]
+    else:
+        return []
 
 
-def get_recommendations_for_user(user_id):
-    return ["Recipe X", "Recipe Y", "Recipe Z", "Recipe P", "Recipe Q", "Recipe R", "Recipe S", "Recipe T", "Recipe U", "Recipe V", "Recipe W"]
+def get_user_data(user_id):
+    response = requests.get(f'http://127.0.0.1:8000/recommend/{user_id}')
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"Error": "Error fetching data."}
 
 
 def main():
     st.title("Food.com Recipe Recommendation Engine")
     st.write("Find the best recipes tailored for you based on your past preferences!")
-    user_id = st.selectbox("Select User ID", [1, 2, 3, 4, 5])
+    available_users = get_available_users()
+    if not available_users:
+        st.write("No available users.")
+        return
+    user_id = st.selectbox("Select User ID", available_users)
     if st.button("Submit"):
+        user_data = get_user_data(user_id)
+        if "Error" in user_data:
+            st.write("Error fetching data from the server.")
+            return
         col1, col2 = st.columns(2)
         col1.subheader(f"Recipes Rated by User {user_id}")
-        user_recipes = get_user_rated_recipes(user_id)
+        past_rated_recipes = user_data.get("past_rated_recipes", [])
         scrollable_rated_recipes = '<div style="height: 250px; overflow-y: auto;">'
-        for recipe in user_recipes:
+        for recipe in past_rated_recipes:
             scrollable_rated_recipes += f"<p>{recipe}</p>"
         scrollable_rated_recipes += "</div>"
         col1.write(scrollable_rated_recipes, unsafe_allow_html=True)
         col2.subheader(f"Recommended Recipes for User {user_id}")
-        recommended_recipes = get_recommendations_for_user(user_id)
+        recommended_recipes = user_data.get("recommended_recipes", [])
         scrollable_recommended_recipes = '<div style="height: 250px; overflow-y: auto;">'
         for recipe in recommended_recipes:
             scrollable_recommended_recipes += f"<p>{recipe}</p>"
